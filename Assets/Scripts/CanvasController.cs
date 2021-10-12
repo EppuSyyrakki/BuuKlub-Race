823694e2ac68c42755313d11c8fef49654900815
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2021-10-12 10:16:30 +0300

    StartCanvas prefab

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BKRacing
+{
+	public class CanvasController : MonoBehaviour
+	{
+		public void StartGame()
+		{
+			Debug.Log("Start game pressed");
+		}
+
+		public void RestartGame()
+		{
+			Debug.Log("Restart game pressed");
+		}
+	}
+}
